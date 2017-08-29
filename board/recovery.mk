c19757e9aedab0fa027b3e12ed0ee233dcf5e4e2
Scott Warner <Tortel1210@gmail.com>
2017-08-29 16:25:52 -0400

    DNM: kipper: Add fstab and configs for TWRP
    Change-Id: Ie47117a691b1c57cf52cce20d900197d5ab9a938

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,2 +1,20 @@
+# RECOVERY_VARIANT := twrp
+
+ifeq ($(RECOVERY_VARIANT),twrp)
+TARGET_RECOVERY_FSTAB := device/wileyfox/kipper/rootdir/recovery/twrp.fstab
+DEVICE_RESOLUTION := 1080x1920
+TW_THEME := portrait_mdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := "160"
+#TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_NO_LEGACY_PROPS := true
+TW_EXTRA_LANGUAGES := true
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_NTFS_3G := true
+#TWRP_EVENT_LOGGING := true
+else
 # Recovery
 TARGET_RECOVERY_FSTAB := device/wileyfox/kipper/rootdir/etc/fstab.qcom
+endif
