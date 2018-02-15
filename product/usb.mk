dcf407b9228c0127d514fd4d61d02800908ca086
jrior001 <jriordan001@gmail.com>
2018-02-14 21:23:17 -0500

    kipper: move permissions to vendor
    Change-Id: Ibdc83b3a6a6367cebbfe7be4d1b97eef90cc8e8a

--- product/usb.mk
+++ product/usb.mk
@@ -1,7 +1,7 @@
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/vendor/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/vendor/etc/permissions/android.hardware.usb.host.xml
 
 # USB ID
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
