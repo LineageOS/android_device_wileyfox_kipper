797d515e059a9ec7d94653c1b65ae30e63816a72
Michael Bestas <mkbestas@lineageos.org>
2018-03-23 20:47:04 +0200

    kipper: Use common aw2013 HIDL light HAL
    Change-Id: Iaf1d96e3296f5f7340dae7495afe96998a42519b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,9 +55,6 @@ BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := lineageos_kipper_defconfig
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # LineageHW
 BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/lineagehw
 
