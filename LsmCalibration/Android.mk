991021fcf32dce9ff102c7c8955117e73b57233e
jrior001 <jriordan001@gmail.com>
2018-08-15 22:53:01 -0400

    sensors: use LOCAL_PRIVATE_PLATFORM_APIS for LsmCalibration
    Change-Id: Ie95e727fef18dc56fc72cc1ca32f56d0a6da5c3d

--- LsmCalibration/Android.mk
+++ LsmCalibration/Android.mk
@@ -7,5 +7,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := LsmCalibration
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
